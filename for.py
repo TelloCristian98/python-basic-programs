312f799ed89a27c901020e5eb3c8cfc30217b191
Cristian Tello <cristian.andres.t98@gmail.com>
2021-02-03 10:29:07 -0500

    Agregando todos los programas al repositorio local

---
+++ for.py
@@ -0,0 +1,12 @@
+def run():
+    # for contador in range(1, 1001): #range(1000) quiere
+    #     #decir que va desde 0 a 1000
+    #     #range(valor inicial, valor final - 1) #
+    #     print(contador)
+
+    for i in range(0, 10):
+        print(11 * i)
+
+if __name__ == '__main__':
+    run()
+
---
+++ recorrer.py
@@ -0,0 +1,12 @@
+def run():
+    # nombre = input("Escribe tu nombre: ")
+    # for i in nombre:
+    #     print(i)    
+
+    frase = input("Escriba una frase: ")
+    for caracter in frase:
+        print(caracter.upper()) 
+
+
+if __name__ == '__main__':
+    run()
